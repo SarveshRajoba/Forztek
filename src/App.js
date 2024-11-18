1c2cc683336d2458df716ff815bd65c40613d0f4
Neeraj Magdum <neerajmagdum10@gmail.com>
2024-11-18 16:15:15 +0530

    React project created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-bold text-red-600'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quis quos possimus soluta id, aspernatur omnis excepturi ipsa blanditiis et consequatur, facilis itaque, rerum dolores quidem eligendi molestias dolore perferendis!</h1>
+    </div>
+  );
+}
+
+export default App;
