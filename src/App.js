cc9a650047f2822e3185efc2b88976d2badc64e1
SarveshRajoba <sarvesh.rajoba10@gmail.com>
2024-11-20 00:15:01 +0530

    About us 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import AboutUs from './AboutUs';
-import ContactPage from './ContactPage';
-import SegmentExpertise from './SegmentExpertise';
-import Flashcard from './components/Flashcard';
-import OurOffering from './pages/our_offering/OurOffering';
-import OurProducts from './pages/OurProducts';
+// import AboutUs from './AboutUs';
+// import ContactPage from './ContactPage';
+// import SegmentExpertise from './SegmentExpertise';
+// import Flashcard from './components/Flashcard';
+// import OurOffering from './pages/our_offering/OurOffering';
+// import OurProducts from './pages/OurProducts';
 
-function App() {
-  return (
-    <div className="App">
-      <AboutUs />
-      <SegmentExpertise />
-      <ContactPage />
-      <OurOffering />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <AboutUs />
+//       <SegmentExpertise />
+//       <ContactPage />
+//       <OurOffering />
+//     </div>
+//   );
+// }
