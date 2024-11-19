6e5cec7efd7ef6becda031d675700888eabdacac
SarveshRajoba <132903409+SarveshRajoba@users.noreply.github.com>
2024-11-20 00:06:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
 import AboutUs from './AboutUs';
 import ContactPage from './ContactPage';
 import SegmentExpertise from './SegmentExpertise';
+import Flashcard from './components/Flashcard';
+import OurOffering from './pages/our_offering/OurOffering';
+import OurProducts from './pages/OurProducts';
 
 function App() {
   return (
     <div className="App">
-      // <AboutUs />
-      // <SegmentExpertise />
-      // <ContactPage />
-
+      <AboutUs />
+      <SegmentExpertise />
+      <ContactPage />
+      <OurOffering />
     </div>
   );
 }
 
-export default App;
