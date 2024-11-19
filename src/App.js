0bae11025ad0ad1ad074bcda4ed267e1ce357d15
SarveshRajoba <sarvesh.rajoba10@gmail.com>
2024-11-20 00:03:38 +0530

    try 1 to resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
 import AboutUs from './AboutUs';
 import ContactPage from './ContactPage';
 import SegmentExpertise from './SegmentExpertise';
+import Flashcard from './components/Flashcard';
+import OurOffering from './pages/our_offering/OurOffering';
+import OurProducts from './pages/OurProducts';
 
 function App() {
   return (
@@ -9,9 +11,7 @@ function App() {
       <AboutUs />
       <SegmentExpertise />
       <ContactPage />
-
+      <OurOffering />
     </div>
   );
 }
-
-export default App;
