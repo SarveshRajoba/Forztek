f6e72d3f70fbfbfec02212efa26e071887e81bc4
Neeraj Magdum <neerajmagdum10@gmail.com>
2024-11-19 21:42:00 +0530

    products page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Flashcard from './components/Flashcard';
+ 
+import OurProducts from './pages/OurProducts';
+ 
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-bold text-red-600'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis quis quos possimus soluta id, aspernatur omnis excepturi ipsa blanditiis et consequatur, facilis itaque, rerum dolores quidem eligendi molestias dolore perferendis!</h1>
-    </div>
+      <OurProducts/>  
+     </div>
   );
 }
 
 export default App;
+
---
+++ src/pages/OurProducts.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Flashcard from "../components/Flashcard";
+import logo from "../assets/our_product.png";
+
+const OurProducts = () => {
+  return (
+    <div className=" bg-forztek-blue h-screen flex items-center justify-between px-10">
+      <div>
+        <div class="text-3xl md:text-4xl lg:text-6xl font-bold text-white mt-20 text-left ml-28">
+          <span class="block">Our</span>
+          <span class="block">Products</span>
+        </div>
+        <img src={logo} className="mx-auto" />
+      </div>
+
+      <div className="mr-28">
+        <Flashcard />
+      </div>
+    </div>
+  );
+};
+
+export default OurProducts;
