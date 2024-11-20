63cf96b0c95205c9324c113daf783d39071a48c5
Abhinandan Patil <abhinandanpatil002@gmail.com>
2024-11-20 15:57:49 +0530

    Save local changes before pulling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Flashcard from './components/Flashcard';
-import OurOffering from './pages/our_offering/OurOffering';
-import OurProducts from './pages/OurProducts';
+// import Flashcard from './components/Flashcard';
+// import OurOffering from './pages/our_offering/OurOffering';
+// import OurProducts from './pages/OurProducts';
+import OurOffering from './pages/OurOffering';
 
 function App() {
   return (
