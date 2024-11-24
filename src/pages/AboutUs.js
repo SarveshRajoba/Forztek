b9eaa73adc60ba5008da32b674a751c77fce2861
SarveshRajoba <sarvesh.rajoba10@gmail.com>
2024-11-24 13:21:21 +0530

    changes in about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -8,16 +8,16 @@ const AboutUs = () => {
     <div className="bg-white text-gray-900 px-6 py-6 min-h-screen mx-auto">
       {/* About Section */}
       <div className="mt-20 mb-20 flex flex-col md:flex-row items-center justify-between gap-6">
-     
+
         <video
-          className="flex-shrink-0  object-cover w-1/2 h-auto shadow-neutral-900 shadow-xl"
-          src="/video.mp4" // Replace with your video path
+          className="ml-10 flex-shrink-0  object-cover w-1/2 h-auto shadow-neutral-900 shadow-xl w-[600px] h-[500px]"
+          src="/video_aboutUs.mp4" // Replace with your video path
           autoPlay
           loop
           muted
         ></video>
         {/* About Us Information */}
-        <div className="text-lora mt-0 md:mt-0 max-w-xl text-center md:text-left md:ml-6">
+        <div className="text-lora mt-0 md:mt-0 max-w-xl text-center md:text-left md:ml-6 mr-10">
           {" "}
           {/* Reduced only the left margin */}
           <h1 className="font-serif text-4xl font-bold mb-3">
@@ -25,7 +25,7 @@ const AboutUs = () => {
           </h1>
           <div>
             {/* <div className="w-32 h-1 mt-2 bg-blue-400 rounded-3xl mb-8 mx-auto md:mx-0"></div> */}
-            <Blueline/>
+            <Blueline />
           </div>
           <p className="text-lg text-gray-600 text-justify leading-relaxed mt-4">
             At Forztek, we specialize in engineering solutions for rotating
