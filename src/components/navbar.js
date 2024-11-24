a2aae925e510804b70e5610415957801ce26bf58
chirag <chiragdave1888@gmail.com>
2024-11-24 17:15:35 +0530

    fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="fixed top-0 left-0 w-full backdrop-filter backdrop-blur-lg backdrop-brightness-50 text-white flex items-center justify-between px-8 py-3 z-50">
+      <nav className="fixed top-0 left-0 w-full backdrop-filter backdrop-blur-lg backdrop-brightness-50 text-white flex items-center justify-between px-8 py-3 z-50 max-w-screen">
         <div className="flex items-center">
           <img src="/logo 3.png" alt="Forztek Logo" className="h-10 w-auto" />
         </div>
