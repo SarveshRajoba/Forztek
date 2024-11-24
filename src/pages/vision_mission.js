14e07c0aaca2230c53979b92f606f8cc66059d6c
Abhinandan Patil <abhinandanpatil002@gmail.com>
2024-11-24 11:13:09 +0530

    Blue Line Component is added

--- src/pages/vision_mission.js
+++ src/pages/vision_mission.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Blueline from "../components/blueline";
 
 function VisionMission() {
     return (
         <div className="flex h-screen relative">
             {/* Left Section */}
             <div className="w-1/2 bg-[#201E43] text-white flex flex-col justify-center items-center px-6 py-6">
-                <h1 className="text-5xl font-serif font-semibold mb-8 tracking-wider">VISION</h1>
+                <h1 className="text-5xl font-serif font-semibold mb-0 tracking-wider">VISION</h1>
+                <div className='mb-8'>
+                    <Blueline/>
+                </div>
                 <p className="text-2xl  font-serif leading-relaxed text-center w-[250px]">
                     A future where our solutions contribute to a reliable and sustainable green world.
                 </p>
@@ -13,7 +17,10 @@ function VisionMission() {
 
             {/* Right Section */}
             <div className="w-1/2 bg-white text-black flex flex-col justify-center items-center px-6 py-6">
-                <h1 className="text-5xl  font-serif font-semibold mb-8 tracking-wider">MISSION</h1>
+                <h1 className="text-5xl  font-serif font-semibold  tracking-wider">MISSION</h1>
+                <div className='mb-8'>
+                    <Blueline/>
+                </div>
                 <p className="text-2xl font-serif leading-relaxed text-center w-[250px]">
                     To help customers & be the leader in innovative and sustainable solutions for rotating equipment.
                 </p>
